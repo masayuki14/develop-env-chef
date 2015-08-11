b83f5fff13e9ae34d66d4a4f23f638400bed5c69
Masayuki Morisaki <masayuki144@gmail.com>
2015-08-11 15:04:25 +0900

    rvmでrubyをインストールする

--- chef-repo/site-cookbooks/basic-env/recipes/default.rb
+++ chef-repo/site-cookbooks/basic-env/recipes/default.rb
@@ -180,3 +180,14 @@ end
 #  not_if 'test -L /home/vagrant/xxneorg/httpdocs/scripts/app'
 #end
 
+bash 'install-rvm' do
+  cwd  '/home/vagrant'
+  user 'vagrant'
+  environment 'HOME' => '/home/vagrant'
+  not_if 'which rvm'
+  code <<-EOF
+    gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+    curl -sSL https://get.rvm.io | bash -s stable --ruby
+  EOF
+end
+
